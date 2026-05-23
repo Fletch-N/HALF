420ec695945940b3f175b0239cd5ee3e6ec4a0ca
Fletcher Nichols <fletcher.w.nichols@gmail.com>
2026-05-22 23:06:31 -0500

    Remove abstract HostCommand class from HALF.Host.Commands

--- src/HALF.Host/Commands/HostCommand.cs
+++
@@ -1,8 +0,0 @@
-namespace HALF.Host.Commands;
-
-internal abstract class HostCommand : IHostCommand
-{
-    public abstract CommandDescriptor Descriptor { get; }
-
-    public abstract CommandExecutionResult Execute(string[] args);
-}
